ee0450c7275a594b5a54fa4901db5a8b7936cf50
Mihir Sathe <91172766+weekendproj@users.noreply.github.com>
2021-09-26 23:13:27 -0700

    Data collection script
    This collects data from free e-books from Project Gutenberg.

---
+++ data_prep/prep.py
@@ -0,0 +1,33 @@
+import urllib.request 
+from bs4 import BeautifulSoup
+
+def format_text(unformatted):
+    formatted = ''
+    for c in unformatted.upper():
+        if ord(c) >= 65 and ord(c) <= 90:
+            formatted += c
+    
+    return formatted
+
+
+def get_feature_blocks(url: str):
+    html = urllib.request.urlopen(url)
+    parser = BeautifulSoup(html, 'html.parser')
+
+    for paragraph in parser.find_all("p"):
+        txt = paragraph.text.strip()
+        if len(txt) > 500:
+            yield format_text(txt)
+
+
+if __name__ == '__main__':
+    """
+    This was run as an ipython notebook
+    """
+
+    # Put a project Gutenberg book link here
+    url = 'https://www.gutenberg.org/cache/epub/16728/pg16728-images.html'
+
+    with open('data.txt', 'at') as file:
+        for block in get_feature_blocks(url):
+            file.write(block + '\n')
